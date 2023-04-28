775f079e602b1f8fa6e7909e04da28597a31fb21
mrkicrade <mrkic78@gmail.com>
2023-04-28 08:50:02 +0200

    Day 25: Create a minimal PDF for each Excel file.

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import pandas as pd
 import glob
+from fpdf import FPDF
+from pathlib import Path
 
 feilapaths = glob.glob('invoices/*.xlsx')
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name='Sheet 1')
-    print(df)
+    # print(df)
+    pdf = FPDF(orientation='P', sheet_name="Sheet 1")
+    pdf.add_page()
+    filename = Path(filepath).stem
+    invoice_nr = filename.split('-')[0]
+    pdf.set_font(family="Times", size=16, style='B')
+    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}")
+    pdf.output(f"PDFs/{filename}.pdf")
